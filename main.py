03288ddb86afe7f63739079f76572160b6fb7499
ecrystale <elee19@stuy,edu>
2019-02-24 16:43:12 -0500

    new img

--- main.py
+++ main.py
@@ -37,17 +37,17 @@ x3=500
 y3=0
 count=0
 change=1
-while x1!=x2 and y2!=y1:
+while x1<x2 and y2>y1:
     add_edge(m,x1,y1,0,x2,y2,0)
     add_edge(m,x2,y2,0,x3,y1,0)
     add_edge(m,x3,y1,0,x2,y3,0)
     add_edge(m,x2,y3,0,x1,y1,0)
-    x1+=1
-    y1+=1
-    x2-=1
-    y2-=1
-    x3-=1
-    y3+=1
+    x1+=3
+    y1+=3
+    x2-=3
+    y2-=3
+    x3-=3
+    y3+=3
         
     
 #add_edge(m,124,2,0,400,200,300)
